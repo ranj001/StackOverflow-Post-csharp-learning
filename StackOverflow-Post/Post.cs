68c601dae4da1fd88b86b913abfcf29560e8bb2f
Lafeguera, Ranjit <Ranjit.Lafeguera@analog.com>
2025-10-18 15:15:35 +0800

    Add project files.

---
+++ StackOverflow-Post/Post.cs
@@ -0,0 +1,34 @@
+﻿namespace StackOverflow
+{
+    public class Post
+    {
+        private string Title { get; set; }
+        private string Description{ get; set; }
+        private DateTime CreatedAt { get; set; }
+
+        public int Votes { get; private set; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            CreatedAt = DateTime.Now;
+            Votes = 0;
+        }
+        public void UpVote()
+        {
+            Votes++;
+        }
+        public void DownVote()
+        {
+            Votes--;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}\nDescription: {Description}\nCreated At: {CreatedAt}\nVotes: {Votes}";
+        }
+
+
+    }
+}
---
+++ StackOverflow-Post/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new post
+            var post = new Post("How to implement a voting system in C#?", "I want to create a simple voting system for posts in C#. How can I do that?");
+            
+            // up-vote the post
+            post.UpVote();
+            
+            // down-vote the post
+            post.DownVote();
+
+            // multiple up-votes
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+
+            // display the post details (votes, title, description, created at)
+            Console.WriteLine(post.ToString());
+        }
+    }
+}
